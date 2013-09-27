b398448711478b63afd61bf181e8b5b11a134a65
Ulrik Damm <ulrikdamm@me.com>
2013-09-27 13:12:26 +0200

    Initial Commit

---
+++ Robokit/RKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RKAppDelegate.h
+//  Robokit
+//
+//  Created by Ulrik Damm on 27/9/13.
+//  Copyright (c) 2013 Robocat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Robokit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Robokit
+//
+//  Created by Ulrik Damm on 27/9/13.
+//  Copyright (c) 2013 Robocat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([RKAppDelegate class]));
+	}
+}

c48ce4017e88f82f4bd53055647c6c107113d313
Simon Støvring <simonstoevring@gmail.com>
2014-02-02 16:45:46 +0100

    Enables rate popup on iOS 6

--- Robokit/Source/Shared/RKMacros.h
+++ Robokit/Source/Shared/RKMacros.h
@@ -10,7 +10,7 @@
 #define RK_IS_IPHONE (UI_USER_INTERFACE_IDIOM() != UIUserInterfaceIdiomPad)
 #define RK_IS_IPHONE_5 (RK_IS_IPHONE && UIScreen.mainScreen.bounds.size.height > 480)
 #define RK_IS_RETINA ([[UIScreen mainScreen] respondsToSelector:@selector(scale)] == YES && [[UIScreen mainScreen] scale] == 2.00)
-#define RK_IS_IOS_7 SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")
+#define RK_IS_IOS_7 RK_SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")
 
 #define RK_SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
 #define RK_SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
